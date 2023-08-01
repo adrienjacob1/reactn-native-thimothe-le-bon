cfa77665ba2e5c2275d2560bf49fcce4937e9178
adrien <adrienjacob1111@gmail.com>
2023-08-01 11:06:50 +0200

    Initial Commit

---
+++ app/index.js
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+import { View, Text, ScrollView, SafeAreaView } from 'react-native';
+import { Stack, useRouter } from 'expo-router';
+
+import { images, icons, COLORS, SIZES } from '../constants';
+
+import { Nearbyjobs, Popularjobs, ScreenHeaderBtn, Welcome } from '../components';
+
+
+
+
+export default function Home() {
+
+}
